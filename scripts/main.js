0d94930e6386593659e8a1e263827f2bd972d681
galyaarbel <galyaa10@gmail.com>
2025-07-29 10:46:55 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -18,7 +18,7 @@ function setUserName() {
     setUserName();
   } else {
     localStorage.setItem("name", myName);
-    myHeading.textContent = `Mozilla is cool, ${myName}`;
+    myHeading.textContent = `You won the championship, ${myName}`;
   }
 }
 
